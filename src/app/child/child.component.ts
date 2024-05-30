cb82cece9829d9a3a3e64308ebb83f6824fec23c
AbhishekDB99 <abhishekborhade4@gmail.com>
2024-05-30 15:45:55 +0530

    child parent changes

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,9 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
   @Input() parentData: any;
   @Input() parentArray: any;
+  @Output() childComponentEvent = new EventEmitter();
+  @Output() getEventFromChildComponentWithoutFunc = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {
     console.log(this.parentData);
+  }
+
+  emitFunction(inputValue: any) {
+    this.getEventFromChildComponentWithoutFunc.emit(inputValue);
   }
 }
